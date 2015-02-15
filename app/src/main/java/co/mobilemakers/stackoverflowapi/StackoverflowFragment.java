44e752a30e1af272c72be4658c69c03e7d8d6ba7
Agustín Gugliotta <agustin.mdq.89@gmail.ccom>
2015-02-15 18:46:26 -0300

    Added HttpOk to replace AsyncTask

--- app/src/main/java/co/mobilemakers/stackoverflowapi/StackoverflowFragment.java
+++ app/src/main/java/co/mobilemakers/stackoverflowapi/StackoverflowFragment.java
@@ -12,6 +12,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,6 +37,7 @@ import java.util.List;
 public class StackoverflowFragment extends Fragment {
     Button mButtonLoad;
     TextView mTextViewUsers, mTextViewComments, mTextViewAnswers, mTextViewApiRev;
+
     public StackoverflowFragment() {
     }
 
@@ -39,112 +45,102 @@ public class StackoverflowFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_stackoverflow, container, false);
-        mButtonLoad = (Button)rootView.findViewById(R.id.button_load);
+        wireUpViews(rootView);
+        prepareButton(rootView);
+        return rootView;
+    }
+
+    private void wireUpViews(View rootView) {
         mTextViewAnswers = (TextView)rootView.findViewById(R.id.text_view_answers_data);
         mTextViewUsers = (TextView)rootView.findViewById(R.id.text_view_users_data);
         mTextViewComments = (TextView)rootView.findViewById(R.id.text_view_comments_data);
         mTextViewApiRev = (TextView)rootView.findViewById(R.id.text_view_api_rev_data);
+    }
+
+    private void prepareButton(View rootView) {
+        mButtonLoad = (Button)rootView.findViewById(R.id.button_load);
         mButtonLoad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new FetchDataTask().execute();
+                fetchStatistics();
             }
         });
-        return rootView;
     }
 
-    private class FetchDataTask extends AsyncTask<String,Void,String>{
+    private void fetchStatistics(){
 
-        @Override
-        protected String doInBackground(String... params) {
-            String data = "";
-            try {
-                URL url = constructQuery();
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                try {
-                    String response = readFullResponse(httpURLConnection.getInputStream());
-                    Log.d("Response", response);
-                    data = parseResponse(response);
-                } catch (IOException e) {
+        try {
+            URL url = constructQuery();
+            Request request = new Request.Builder().url(url.toString()).build();
+            OkHttpClient client = new OkHttpClient();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
                     e.printStackTrace();
-                } finally {
-                    httpURLConnection.disconnect();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return data;
-        }
 
-        private String parseResponse(String response) {
-            final String TOTAL_USERS = "total_users";
-            final String TOTAL_COMMENTS = "total_comments";
-            final String TOTAL_ANSWRES = "total_answers";
-            final String API_REVISION = "api_revision";
-            final String ARRAY_NAME = "items";
-
-            List<String> data = new ArrayList<>();
-
-            try {
-                  JSONObject respObject = new JSONObject(response);
-                  JSONArray arrayJson = respObject.getJSONArray(ARRAY_NAME);
-                  respObject = (JSONObject)arrayJson.get(0);
-                  data.add(respObject.get(TOTAL_USERS).toString());
-                  data.add(respObject.get(TOTAL_COMMENTS).toString());
-                  data.add(respObject.get(TOTAL_ANSWRES).toString());
-                  data.add(respObject.get(API_REVISION).toString());
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-
-            return TextUtils.join(",", data);
-        }
-
-        private String readFullResponse(InputStream inputStream) throws IOException {
-            StringBuilder stringBuilder = new StringBuilder();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            String response = "";
-            String line;
-            while ((line = bufferedReader.readLine()) != null){
-                stringBuilder.append(line).append("\n");
-            }
-
-            if (stringBuilder.length() > 0) {
-                response = stringBuilder.toString();
-            }
-
-            return  response;
-        }
-
-        private URL constructQuery() throws MalformedURLException {
-            final String BASE_URL       = "api.stackexchange.com";
-            final String API_VERSION    = "2.2";
-            final String REPOS_ENDPOINT = "info";
-            final String KEY            = "site";
-            final String VALUE          = "stackoverflow";
-
-            Uri.Builder builder = new Uri.Builder();
-            builder.scheme("https").
-                    authority(BASE_URL).
-                    appendPath(API_VERSION).
-                    appendPath(REPOS_ENDPOINT).
-                    appendQueryParameter(KEY,VALUE);
-
-            Uri uri = builder.build();
-            Log.d("URL", uri.toString());
-            return new URL(uri.toString());
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            Log.d("ARRAY DATA", s);
-            String[] data = s.split(",");
-            mTextViewUsers.setText(data[0].toString());
-            mTextViewComments.setText(data[1].toString());
-            mTextViewAnswers.setText(data[2].toString());
-            mTextViewApiRev.setText(data[3].toString());
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    String responseString = response.body().string();
+                    final String[] info = parseResponse(responseString).split(",");
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTextViewUsers.setText(info[0]);
+                            mTextViewComments.setText(info[1]);
+                            mTextViewAnswers.setText(info[2]);
+                            mTextViewApiRev.setText(info[3]);
+                        }
+                    });
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+    private String parseResponse(String response) {
+        final String TOTAL_USERS = "total_users";
+        final String TOTAL_COMMENTS = "total_comments";
+        final String TOTAL_ANSWRES = "total_answers";
+        final String API_REVISION = "api_revision";
+        final String ARRAY_NAME = "items";
+
+        List<String> data = new ArrayList<>();
+
+        try {
+            JSONObject respObject = new JSONObject(response);
+            JSONArray arrayJson = respObject.getJSONArray(ARRAY_NAME);
+            respObject = (JSONObject)arrayJson.get(0);
+            data.add(respObject.get(TOTAL_USERS).toString());
+            data.add(respObject.get(TOTAL_COMMENTS).toString());
+            data.add(respObject.get(TOTAL_ANSWRES).toString());
+            data.add(respObject.get(API_REVISION).toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return TextUtils.join(",", data);
+    }
+
+    private URL constructQuery() throws MalformedURLException {
+        final String BASE_URL       = "api.stackexchange.com";
+        final String API_VERSION    = "2.2";
+        final String REPOS_ENDPOINT = "info";
+        final String KEY            = "site";
+        final String VALUE          = "stackoverflow";
+
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("https").
+                authority(BASE_URL).
+                appendPath(API_VERSION).
+                appendPath(REPOS_ENDPOINT).
+                appendQueryParameter(KEY,VALUE);
+
+        Uri uri = builder.build();
+        Log.d("URL", uri.toString());
+        return new URL(uri.toString());
+    }
+
 }
